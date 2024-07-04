00a24c965253180db57a86854067ed30aedb0c3e
Agustín Reyes <agusrr.data@outlook.com>
2024-07-04 00:21:08 -0600

    feat: Creación de etiqueta para vincular el botón de log in

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// script.js
+
+
+<script>
+// Obtener el botón de inicio de sesión por su ID
+
+  var loginBtnDesktop = document.getElementById('loginBtn');
+
+  // Agregar un evento de clic al botón
+  loginBtn.addEventListener('click', function() {
+    // Redireccionar a login.html
+    window.location.href = 'login.html';
+  });
+</script>
+
+
