b432d865901dc0a02d1e966a08b243d2056e6fe2
Agustín Reyes <agusrr.data@outlook.com>
2024-07-06 08:47:08 -0600

    feat: Actualización 0845, 06 Jun. 2024

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
-// script.js
+alert("hola mundo");
 
 
-<script>
-// Obtener el botón de inicio de sesión por su ID
-
-  var loginBtnDesktop = document.getElementById('loginBtn');
-
-  // Agregar un evento de clic al botón
-  loginBtn.addEventListener('click', function() {
-    // Redireccionar a login.html
-    window.location.href = 'login.html';
-  });
-</script>
 
 
